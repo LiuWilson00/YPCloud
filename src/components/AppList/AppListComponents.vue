3a900770a64c4f315b966b9374f6fa60a8386614
liuwei811007@gmail.com <liuwei811007@gmail.com>
2020-03-18 14:45:39 +0800

    modify qapp

--- src/components/AppList/AppListComponents.vue
+++ src/components/AppList/AppListComponents.vue
@@ -16,59 +16,10 @@ export default {
   name: "appList",
   data() {
     return {
-      appList: [
-        {
-          appId: 1,
-          name: "Flow Builder",
-          order: 0,
-          iconName: "Flow_Builder",
-          iconUrl: "https://i.imgur.com/3SswLvz.jpg"
-        },
-        {
-          appId: 2,
-          name: "YOLO",
-          order: 10,
-          iconName: "YOLO",
-          iconUrl: "https://i.imgur.com/fIR80RQ.png"
-        },
-        {
-          appId: 3,
-          name: "YOLO",
-          order: 10,
-          iconName: "YOLO",
-          iconUrl: "https://i.imgur.com/fIR80RQ.png"
-        },
-        {
-          appId: 4,
-          name: "YOLO_Wilson",
-          order: 10,
-          iconName: "YOLO",
-          iconUrl: "https://i.imgur.com/fIR80RQ.png"
-        },
-        {
-          appId: 5,
-          name: "YOLO",
-          order: 10,
-          iconName: "YOLO",
-          iconUrl: "https://i.imgur.com/fIR80RQ.png"
-        },
-        {
-          appId: 6,
-          name: "YOLO",
-          order: 10,
-          iconName: "YOLO",
-          iconUrl: "https://i.imgur.com/fIR80RQ.png"
-        },
-        {
-          appId: 7,
-          name: "YOLO",
-          order: 10,
-          iconName: "YOLO",
-          iconUrl: "https://i.imgur.com/fIR80RQ.png"
-        }
-      ]
+      
     };
   },
+  props:["appList"],
   components: {
     AppItem
   }
--- src/views/qapp.vue
+++ src/views/qapp.vue
@@ -2,7 +2,7 @@
   <div class="qapp">
     <HeaderBar class="header-bar" />
     <UserInfo class="user-info" />
-    <AppList />
+    <AppList :appList="appList" />
     <v-card-subtitle>System Status | Pivacy Policy | Terms & Conditions | Copyright © 2020 YP Cloud. All rights reserved</v-card-subtitle>
   </div>
 </template>
@@ -27,6 +27,61 @@ import UserInfo from "@/components/UserInfo/UserInfoComponents.vue";
 import AppList from "@/components/AppList/AppListComponents.vue";
 export default {
   name: "qapp",
+  data() {
+    return {
+      appList: [
+        {
+          appId: 1,
+          name: "Flow Builder",
+          order: 0,
+          iconName: "Flow_Builder",
+          iconUrl: "https://i.imgur.com/3SswLvz.jpg"
+        },
+        {
+          appId: 2,
+          name: "YOLO",
+          order: 10,
+          iconName: "YOLO",
+          iconUrl: "https://i.imgur.com/fIR80RQ.png"
+        },
+        {
+          appId: 3,
+          name: "YOLO",
+          order: 10,
+          iconName: "YOLO",
+          iconUrl: "https://i.imgur.com/fIR80RQ.png"
+        },
+        {
+          appId: 4,
+          name: "YOLO_Wilson",
+          order: 10,
+          iconName: "YOLO",
+          iconUrl: "https://i.imgur.com/fIR80RQ.png"
+        },
+        {
+          appId: 5,
+          name: "YOLO",
+          order: 10,
+          iconName: "YOLO",
+          iconUrl: "https://i.imgur.com/fIR80RQ.png"
+        },
+        {
+          appId: 6,
+          name: "YOLO",
+          order: 10,
+          iconName: "YOLO",
+          iconUrl: "https://i.imgur.com/fIR80RQ.png"
+        },
+        {
+          appId: 7,
+          name: "YOLO",
+          order: 10,
+          iconName: "YOLO",
+          iconUrl: "https://i.imgur.com/fIR80RQ.png"
+        }
+      ]
+    };
+  },
   components: {
     HeaderBar,
     UserInfo,
